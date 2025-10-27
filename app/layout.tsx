f20c56c621ddda923d10f7f435a7a2a124343cd2
Imran Molla Joy <imranmollajoy@gmail.com>
2025-10-28 00:34:00 +0600

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Student Association of Satkhira, PSTU",
   description: "",
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` ${roboto.variable} antialiased`}>
         <div className="min-h-screen">{children}</div>
         <Footer />
       </body>
